9caa6cb29caf04efc61313536c5cf7a910ee3ab8
zhangkaidi1 <zhangkaidi@jd.com>
2020-07-27 16:46:31 +0800

    add file

---
+++ .vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: '基础业务研发部-前端基础框架',
+    description: 'Just playing around',
+    themeConfig: {
+        displayAllHeaders: true // 默认值：false
+    },
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' }
+        ],
+        sidebar: {
+            '/': [
+                '',
+                '/guide/'
+            ]
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# fe-baseUI
-基础组件库
+
+# 组件指南
+
+## 弹层组件
+```
+console.log(1)
+```
+## Tab组件
+```
+console.log(1)
+```
+## 通知组件
+```
+console.log(1)
+```
+## 商品列表
+```
+console.log(1)
+```
+## Toast
+```
+console.log(1)
+```
+## 加载更多
+```
+console.log(1)
+```
